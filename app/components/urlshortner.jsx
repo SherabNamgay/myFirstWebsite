b7d03379777cc3c73e7e8325800607f92e1fae70
Sherab Namgay <144956521+SherabNamgay@users.noreply.github.com>
2024-02-27 15:01:07 +0600

    added usestate

--- app/components/urlshortner.jsx
+++ app/components/urlshortner.jsx
@@ -1,19 +1,17 @@
-export default function UrlShortner(){
-    return(
-        <>
-        <div className="text-center">
-				<h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-					URL shortner
-				</h1>
-				<input 
-                type="text"
-                name="URL"
-                id= "urlInput"
-                className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                placeholder="Enter your URL">
-                </input>
+// export default function UrlShortner(){
+//     return(
+//         <>
+//         <div className="text-center">
+// 				<h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+// 					URL shortner
+// 				</h1>
+// 				<input 
+//                 type="text"
+//                 className="block w-fit rounded-md border-4 text-gray-900 placeholder:text-gray-400"
+//                 placeholder="Enter your URL">
+//                 </input>
                 
-			</div>
-        </>
-    )
-}
+// 			</div>
+//         </>
+//     )
+// }
--- app/page.js
+++ app/page.js
@@ -1,40 +1,45 @@
-import UrlShortner from "./components/urlshortner"
+"use client"
 
-const URL_LINK="https://shrtlnk.dev/api/v2/link"
-const API_KEY= process.env.API_KEY
-// var inputElement=document.getElementById("userInput")
-// var inputValue =inputElement.value
-var urlToShorten={
-  url:"https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/solutions/javascript?filter=me&sort=best_practice&invalids=false"
-}
-async function getData() {
-	const res = await fetch(URL_LINK,{
-    method:"POST",
-    headers:{
-      "api-key": API_KEY,
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(urlToShorten),
-  })
-  if (!res.ok) {
-		throw new Error('Failed to fetch data')
-	}
+import { useState } from "react"
 
-	return res.json()
-}
+export default function Home() {
+	const [quotes, setQuotes] = useState([])
+	const [text, setText] = useState("Hello")
 
 
-export default async function Home() {
-	const data = await getData()
-	console.log(data)
 	return (
-		<>
-      <UrlShortner
-     
-      />
-
-		</>
+    <div className="h-screen bg-cover bg-center bg-[url('/bgimage.jpg')]">
+      <div className="mx-auto w-fit mt-80">
+        <div className="text-4xl text-red-500 text-center">
+          {quotes.map((quote) => {
+            return (
+              <p class>
+                {quote}
+              </p>
+            )
+          })}
+        </div>
+        <input
+          className="text-2xl text-black mt-12 border-2 border-green-300 rounded-full bg-white hover:bg-sky-200"
+          type="text"
+          placeholder="Enter text here"
+          onChange={(event) => setText(event.target.value)}
+        />
+        <button
+          className="text-2xl ml-8 font-bold border-4 px-2 border-white rounded-full bg-sky-500 hover:bg-sky-700 "
+          onClick={() => {
+            const newQuotes = quotes.concat([text])
+            setQuotes(newQuotes)
+            setText("")
+          }}
+        >
+          Save Text
+        </button>
+        <div className="text-8xl text-green-500 text-center mt-12">
+          {text}
+        </div>
+      </div>
+    </div>
 	)
 }
 
